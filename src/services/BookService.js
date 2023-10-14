e9bdbcc47d705953218bbb30122160a75ae7bde0
Sarah Bernardino <sarahberna799@gmail.com>
2023-10-14 15:33:23 -0300

    Update BookService.js
    criação de duas constantes e exportação exemplo

--- src/services/BookService.js
+++ src/services/BookService.js
@@ -1,3 +1,13 @@
 const model = require('../infra/database/models/Book');
 
 //TODO: Criar service da aplicação
+const getAll = async () =>{
+  return await model.find();
+};
+const getById = async (id) =>{
+  return await model.findById(id);
+};
+module.exports = {
+  getAll,
+  getById,
+};
